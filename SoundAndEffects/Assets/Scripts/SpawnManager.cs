c10026300dff167c59b8b71b6a341e50b41e6855
MiningExpert99 <gnickolas67@gmail.com>
2024-12-12 14:17:29 -0500

    3.2 done

--- SoundAndEffects/Assets/Scripts/SpawnManager.cs
+++ SoundAndEffects/Assets/Scripts/SpawnManager.cs
@@ -6,18 +6,18 @@ public class SpawnManager : MonoBehaviour
     private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float startDelay = 2;
     private float repeatRate = 2;
+    private PlayerController playerControllerScript;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
-    void SpawnObstacle ()
+    void SpawnObstacle()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
     }
 }
