51048df26406d2e3bbc9e201b3172a078c7599e8
MiningExpert99 <gnickolas67@gmail.com>
2024-12-19 14:10:28 -0500

    3.4 done

--- SoundAndEffects/Assets/Scripts/PlayerController.cs
+++ SoundAndEffects/Assets/Scripts/PlayerController.cs
@@ -7,31 +7,42 @@ public class PlayerController : MonoBehaviour
     public float jumpForce;
     public float gravityModifier;
     private Animator playerAnim;
+    private AudioSource playerAudio;
     void Start()
     {
+        playerAudio = GetComponent<AudioSource>();
         playerRb = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
     public bool isOnGround = true;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
             playerAnim.SetTrigger("Jump_tring");
         }
     }
     public bool gameOver = false;
+    public ParticleSystem explosionParticle;
     private void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.CompareTag("Ground"))
         {
+            dirtParticle.Play();
             isOnGround = true;
         }
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
+            playerAudio.PlayOneShot(crashSound, 1.0f);
+            dirtParticle.Stop();
+            explosionParticle.Play();
             Debug.Log("Game Over!");
             gameOver = true;
             playerAnim.SetBool("Death_b", true);
